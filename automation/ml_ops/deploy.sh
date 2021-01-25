2a912bbcdd15e555497f4c26a3258937dfcdb75c
EC2 Default User <ec2-user@ip-172-31-50-207.ec2.internal>
2021-01-25 01:02:55 +0000

    new template and deploy script

--- automation/ml_ops/deploy.sh
+++ automation/ml_ops/deploy.sh
@@ -11,9 +11,8 @@ brew --version
 brew tap aws/tap
 brew install aws-sam-cli
 sam --version
-git clone https://github.com/luseloso/amazon-personalize-immersion-day.git
 cd /home/ec2-user/SageMaker/amazon-personalize-immersion-day/automation/ml_ops/
-sam deploy --template-file template.yaml --stack-name id-ml-ops --capabilities CAPABILITY_IAM --s3-bucket ${SAMArtifactsBucket}
+sam deploy --template-file template.yaml --stack-name id-ml-ops --capabilities CAPABILITY_IAM --s3-bucket $1
 bucket=$(aws cloudformation describe-stacks --stack-name id-ml-ops --query "Stacks[0].Outputs[?OutputKey=='InputBucketName'].OutputValue" --output text)
 aws s3 sync ./data s3://$bucket
 aws s3 cp ./params.json s3://$bucket 
