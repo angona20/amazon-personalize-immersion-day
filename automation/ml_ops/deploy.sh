d01f42db0e927ae3549a7ae09abf20f35b1fd913
EC2 Default User <ec2-user@ip-172-31-50-207.ec2.internal>
2021-01-25 01:04:56 +0000

    new naming for the example mlops will deploy

--- automation/ml_ops/deploy.sh
+++ automation/ml_ops/deploy.sh
@@ -11,7 +11,6 @@ brew --version
 brew tap aws/tap
 brew install aws-sam-cli
 sam --version
-cd /home/ec2-user/SageMaker/amazon-personalize-immersion-day/automation/ml_ops/
 sam deploy --template-file template.yaml --stack-name id-ml-ops --capabilities CAPABILITY_IAM --s3-bucket $1
 bucket=$(aws cloudformation describe-stacks --stack-name id-ml-ops --query "Stacks[0].Outputs[?OutputKey=='InputBucketName'].OutputValue" --output text)
 aws s3 sync ./data s3://$bucket
