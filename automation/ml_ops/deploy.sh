42e1cabbc932ea79a578d2971b836dc24505a2f7
EC2 Default User <ec2-user@ip-172-31-50-207.ec2.internal>
2021-01-25 03:46:19 +0000

    new EE CFN

--- automation/ml_ops/deploy.sh
+++ automation/ml_ops/deploy.sh
@@ -2,14 +2,7 @@
 sudo service docker start
 sudo usermod -a -G docker ec2-user
 docker ps
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
-test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
-test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
-echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
-brew --version
-brew tap aws/tap
-brew install aws-sam-cli
+pip install aws-sam-cli
 sam --version
 sam deploy --template-file template.yaml --stack-name id-ml-ops --capabilities CAPABILITY_IAM --s3-bucket $1
 bucket=$(aws cloudformation describe-stacks --stack-name id-ml-ops --query "Stacks[0].Outputs[?OutputKey=='InputBucketName'].OutputValue" --output text)
