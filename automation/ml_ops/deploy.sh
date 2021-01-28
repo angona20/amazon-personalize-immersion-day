f1c59a7d96b184a713ea02fcc9c325080ed9d9b7
EC2 Default User <ec2-user@ip-172-31-50-207.ec2.internal>
2021-01-28 18:32:59 +0000

    adding domains

--- automation/ml_ops/deploy.sh
+++ automation/ml_ops/deploy.sh
@@ -6,5 +6,5 @@ pip install aws-sam-cli
 sam --version
 sam deploy --template-file template.yaml --stack-name id-ml-ops --capabilities CAPABILITY_IAM --s3-bucket $1
 bucket=$(aws cloudformation describe-stacks --stack-name id-ml-ops --query "Stacks[0].Outputs[?OutputKey=='InputBucketName'].OutputValue" --output text)
-aws s3 sync ./data s3://$bucket
-aws s3 cp ./params.json s3://$bucket 
+aws s3 sync ./domain/$2/data s3://$bucket
+aws s3 cp ./domain/$2/params.json s3://$bucket 
